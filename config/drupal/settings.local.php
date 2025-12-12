f19d3b83cb249ad68c6ec194ece68fecf17cdb4d
Stephen <dev@stephenbusic.com>
2025-12-12 11:06:35 -0500

    fixed local settings

--- config/drupal/settings.local.php
+++ config/drupal/settings.local.php
@@ -12,17 +12,20 @@ $settings['trusted_host_patterns'] = [
  * Reverse proxy configuration for Traefik
  */
 $settings['reverse_proxy'] = TRUE;
-$settings['reverse_proxy_addresses'] = [
-  '172.16.0.0/12',
-  '10.0.0.0/8',
-];
+$settings['reverse_proxy_addresses'] = ['172.16.0.0/12', '10.0.0.0/8'];
 
 /**
- * Tell Drupal to respect X-Forwarded-Proto from Traefik
+ * Tell Drupal the connection is HTTPS when behind Traefik
  */
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
   $_SERVER['HTTPS'] = 'on';
 }
+
+/**
+ * IMPORTANT: Disable any forced HTTPS redirects
+ * Let Traefik handle HTTPS - Drupal should NOT redirect
+ */
+$settings['https'] = FALSE;
 
 /**
  * Skip file system permissions check (Docker volumes)
